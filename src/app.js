09d00557e2a65f54b444eb2e2e82fd12728ebafb
Bryan García <bryan1997st@gmail.com>
2021-03-19 19:37:20 -0500

    Se generó el codigo del generador de excusas con funciones y emojis

--- src/app.js
+++ src/app.js
@@ -7,5 +7,23 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let quien = ["Mi perro", "Mi mamá", "Mi hermanito"];
+  let accion = ["se comío", "tiró", "daño", "mojó", "explotó"];
+  let que = ["mi tarea", "mi comida", "la laptop", "el teléfono"];
+  let cuando = ["esta mañana", "ayer", "justo antes de salir", "anoche"];
+  let emoji = ["😱", "😢", "😦", "🙁", "😟"];
+
+  function fraseRandom() {
+    let quienRandom = Math.floor(Math.random() * quien.length);
+    let accionRandom = Math.floor(Math.random() * accion.length);
+    let queRandom = Math.floor(Math.random() * que.length);
+    let cuandoRandom = Math.floor(Math.random() * cuando.length);
+    let emojiRandom = Math.floor(Math.random() * emoji.length);
+
+    let oracionRandom = `${quien[quienRandom]} ${accion[accionRandom]} ${que[queRandom]} ${cuando[cuandoRandom]} ${emoji[emojiRandom]}  `;
+
+    return oracionRandom;
+  }
+  let excuse = fraseRandom();
+  document.querySelector("#excuse").innerHTML = excuse;
 };
